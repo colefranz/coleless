c3b9bb7a06082bd2702362162649be7873d64abd
unknown <sararathje@gmail.com>
2017-10-18 20:14:22 -0600

    Added some comments to getParamByName, and changed the if to check for null instead of just 'not'. Feel free to change back if this doesn't work as expected and I'm totally off the rocker.

--- client/utils/getParamByName.jsx
+++ client/utils/getParamByName.jsx
@@ -1,9 +1,22 @@
 export default function getParameterByName(name) {
+  console.log('getParamByName has been called with name = ' + name);
   var url = window.location.href;
+
+  // Replace all instances of a bunch of brackets with the last match
+  // TODO: Replace $& with something more robust. See:
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastMatch
   name = name.replace(/[\[\]]/g, "\\$&");
+
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      // Will execute a search for a match in the URL. If not found, value of results will be null.
       results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+
+  if (results === null) {
+    return null;
+  } else if (_.isEmpty(results[2])) {
+    return '';
+  } else {
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
 }
+
